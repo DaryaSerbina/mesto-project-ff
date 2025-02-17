0144db75d2a90875da544b753ba116425dba10b8
DaryaSerbina <daria.srbn@yandex.ru>
2025-02-17 16:00:54 +0300

    test

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,32 @@
 const initialCards = [
-    {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    },
-    {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-    },
-    {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-    },
-    {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-    },
-    {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-    },
-    {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-    }
+  {
+    name: "Сочи",
+    link: "https://i.postimg.cc/Y0V3jYFs/photo-2025-02-11-11-53-33.jpg",
+    alt: "Тисо-Самшитовая роща в Сочи",
+  },
+  {
+    name: "Саратовская область",
+    link: "https://i.postimg.cc/Qt8SzXs0/photo-2023-10-03-23-13-45.jpg",
+    alt: "Утёс Синее Лбище в Саратовской области",
+  },
+  {
+    name: "Тольятти",
+    link: "https://i.postimg.cc/TYL0FNMM/photo-2024-11-10-00-33-12.jpg",
+    alt: "Замок Гарибальди в Тольятти",
+  },
+  {
+    name: "Алтай",
+    link: "https://i.postimg.cc/ZKGHSP49/photo-2024-07-06-11-05-50.jpg",
+    alt: "Чулышманская долина в Алтае",
+  },
+  {
+    name: "Курильские острова",
+    link: "https://i.postimg.cc/VkXRLSkK/photo-2024-03-30-14-12-05.jpg",
+    alt: "Вулкан Алаид на Курильском острове",
+  },
+  {
+    name: "Мурманск",
+    link: "https://i.postimg.cc/tRmDrQcN/photo-2023-12-27-20-47-17.jpg",
+    alt: "Северное сияние в Мурманске",
+  },
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
-// @todo: Темплейт карточки
-
 // @todo: DOM узлы
+const content = document.querySelector(".content");
+const placesList = content.querySelector(".places__list");
+const addButton = content.querySelector(".profile__add-button");
 
 // @todo: Функция создания карточки
 
+function createCard(cardData, deleteCard) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector(".card__image").src = cardData.link;
+  cardElement.querySelector(".card__image").alt = cardData.alt;
+  cardElement.querySelector(".card__title").textContent = cardData.name;
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", () => deleteCard(cardElement));
+  return cardElement;
+}
+
 // @todo: Функция удаления карточки
 
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+addButton.addEventListener("click", () => {
+  initialCards.forEach((cardData) => {
+    const newCard = createCard(cardData, deleteCard);
+    placesList.append(newCard);
+  });
+});
